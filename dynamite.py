5384db53294bc17ea4c93dfc3b5f2276d1b2652b
SHA2770 <134336163+DK27ss@users.noreply.github.com>
2024-04-11 18:49:44 +0200

    dynamite.py

--- dynamite.py
+++ dynamite.py
@@ -1,111 +1,127 @@
-import re
-import requests
+
+
+#   Copyright 2024. SUPERPOSE INT. All rights reserved.
+#   ___    A
+#   | |   {*}       This vulnerability recognition software, including all source code, documentation and associated files, is the exclusive property of @makaki22. Any unauthorized
+#   | |  __V__      reproduction, distribution or modification of this software is strictly prohibited.
+#   |_|o_|%%%|0_    This software is provided "as is" without warranty of any kind, either express or implied, including, but not limited to, the implied warranties of merchantability, 
+#      |       |    fitness for a particular purpose and non-infringement. In no event shall @makaki22 be liable for any direct, indirect, incidental, special or consequential damages arising 
+#      |       |    out of the use or inability to use this software, even if advised of the possibility of such damages.
+#      |_______|
+#                   Use of this software is subject to the terms and conditions of the applicable license agreement provided with the software.
+#                   By using this software, you agree to be bound by the terms of this license.
+#
+#                   If you have any questions about this software or require additional permissions, please contact @makaki22.
+#
+
+import os
+import subprocess
 import colorama
 from colorama import Fore
-import subprocess
-import time
 
-print("""
-          ____
-         '-..-'  @makaki22    .-.
-        ___||___           .-/ /-.
-       /_______/|         / / / /   
-       |       ||        / / / /
-       |   o   |/       / / / / 
-       '---`(--' 0x1   />>=<
-      """)
-
-api_key = '9VWN8UD5TVYKAVMN1IVXJ2QRVR3HHDAFSB'
-
-contract_address = input("TARGET CONTRACT >> ")
-
-account_address = (contract_address)
-
-url_transfers = f'https://api.etherscan.io/api?module=account&action=tokentx&contractaddress={contract_address}&address={account_address}&page=1&offset=100&sort=asc&apikey={api_key}'
-
-response_transfers = requests.get(url_transfers)
-data_transfers = response_transfers.json()
-
-if data_transfers['status'] == "1":
-    token_transfers = data_transfers['result']
-
-    token_balances = {}
-
-    for tx in token_transfers:
-        token_symbol = tx['tokenSymbol']
-        token_amount = int(tx['value']) / (10 ** int(tx['tokenDecimal']))
-
-        if token_symbol in token_balances:
-            token_balances[token_symbol] += token_amount
-        else:
-            token_balances[token_symbol] = token_amount
-
-    print("Check" + Fore.MAGENTA + " >> ", account_address, ":" + Fore.WHITE)
-
-    for symbol, balance in token_balances.items():
-        print("Symbol: " + Fore.CYAN + f"{symbol}" + Fore.WHITE + " | " + Fore.WHITE + "Token Balance: " + Fore.CYAN + f"{balance}" + Fore.WHITE)
-
-    url_contract_details = f'https://api.etherscan.io/api?module=contract&action=getsourcecode&address={contract_address}&apikey={api_key}'
-    response_contract_details = requests.get(url_contract_details)
-    data_contract_details = response_contract_details.json()
-
-    if data_contract_details['status'] == "1":
-        source_code = data_contract_details['result'][0]['SourceCode']
-        match = re.search(r"pragma solidity\s+([^\s]+);", source_code)
-        if match:
-            solidity_version = match.group(1)
-            print("Solidity Compiler:", solidity_version)
-
-            time.sleep(2)
-
-            print("")
-            print("")
-            print(Fore.MAGENTA + "[+] " + Fore.YELLOW + "CONTRACT SOURCE CODE" + Fore.CYAN)
-            print("")
-            print("")
-            print(source_code)
-            print("")
-            print("")
-
-            save_choice = input(Fore.YELLOW + "Save contract source code [.sol] ? (Y/n) ").strip().lower() or 'y'
-            if save_choice == 'y':
-                filename = contract_address + ".sol"
-                with open(filename, "w") as f:
-                    f.write(source_code)
-                print(Fore.CYAN + "contract source code saved at " + Fore.CYAN + f"{filename}" + Fore.WHITE)
-            else:
-                print(Fore.RED + "[ERROR] " + Fore.YELLOW + "contract source code not saved !")
-
-            solidity_version_numbers = re.findall(r'\d+\.\d+\.\d+', solidity_version)
-            if solidity_version_numbers:
-                solidity_version = solidity_version_numbers[0]
-                install_choice = input(Fore.YELLOW + f"Download solidity compiler {solidity_version}? (Y/n) ").strip().lower() or 'y'
-                if install_choice == 'y':
-                    subprocess.run(["solc-select", "install", solidity_version])
-                    print(Fore.YELLOW + f"{solidity_version}" + Fore.CYAN + " solidity compiler successfully download." + Fore.WHITE)
-                else:
-                    print(Fore.RED + "[ERROR] " + Fore.YELLOW + "solidity compiler not download !")
-
-                use_choice = input(Fore.YELLOW + "Use solidity compiler " + Fore.CYAN + f"{solidity_version} " + Fore.WHITE +"? (Y/n) ").strip().lower() or 'y'
-                if use_choice == 'y':
-                    subprocess.run(["solc-select", "use", solidity_version])
-                    print(Fore.YELLOW + f"{solidity_version}" + Fore.CYAN + " solidity compiler successfully added." + Fore.WHITE)
-                else:
-                    print(Fore.RED + "[ERROR] " + Fore.YELLOW + "solidity compiler not added !")
-
-                slither_choice = input("Run" + Fore.MAGENTA + " slither analyzer " + Fore.MAGENTA + "on the locally saved .sol file? (Y/n) ").strip().lower() or 'y'
-                if slither_choice == 'y':
-                    subprocess.run(["slither", filename])
-                    print(Fore.GREEN + "[200 OK] " + Fore.WHITE)
-                else:
-                    print(Fore.RED + "[ERROR] " + Fore.YELLOW + "slither not executed!")
-
-            else:
-                print("Invalid Solidity compiler version")
-        else:
-            print("No pragma solidity declaration found in the contract source code.")
+def call_network_eth(OxETH):
+    OxETH_path = os.path.join("network", OxETH)
+    if os.path.exists(OxETH_path):
+        subprocess.run(["python", OxETH_path])
     else:
-        print("Error retrieving contract details:", data_contract_details['message'])
+        print(f"'{OxETH}' was not found in the 'network' folder.")
 
-else:
-    print("Data recovery error:", data_transfers['message'])
+def call_network_avax(OxAVAX):
+    OxAVAX_path = os.path.join("network", OxAVAX)
+    if os.path.exists(OxAVAX_path):
+        subprocess.run(["python", OxAVAX_path])
+    else:
+        print(f"'{OxAVAX}' was not found in the 'network' folder.")
+
+def call_network_base(OxBASE):
+    OxBASE_path = os.path.join("network", OxBASE)
+    if os.path.exists(OxBASE_path):
+        subprocess.run(["python", OxBASE_path])
+    else:
+        print(f"'{OxBASE}' was not found in the 'network' folder.")
+
+def call_network_bsc(OxBSC):
+    OxBSC_path = os.path.join("network", OxBSC)
+    if os.path.exists(OxBSC_path):
+        subprocess.run(["python", OxBSC_path])
+    else:
+        print(f"'{OxBSC}' was not found in the 'network' folder.")
+
+def call_network_cro(OxCRO):
+    OxCRO_path = os.path.join("network", OxCRO)
+    if os.path.exists(OxCRO_path):
+        subprocess.run(["python", OxCRO_path])
+    else:
+        print(f"'{OxCRO}' was not found in the 'network' folder.")
+
+def call_network_poly(OxPOLY):
+    OxPOLY_path = os.path.join("network", OxPOLY)
+    if os.path.exists(OxPOLY_path):
+        subprocess.run(["python", OxPOLY_path])
+    else:
+        print(f"'{OxPOLY}' was not found in the 'network' folder.")
+
+
+
+def call_ethereum_network():
+    #subprocess.run(["python", "network/0xEth.py"])
+    call_network_eth("0xEth.py")
+def call_avax_network():
+    #subprocess.run(["python", "network/0xAvax.py"])
+    call_network_eth("0xAvax.py")
+def call_base_network():
+    #subprocess.run(["python", "network/0xBase.py"])
+    call_network_eth("0xBase.py")
+def call_bsc_network():
+    #subprocess.run(["python", "network/0xBsc.py"])
+    call_network_eth("0xBsc.py")
+def call_cronos_network():
+    #subprocess.run(["python", "network/0xCro.py"])
+    call_network_eth("0xCro.py")
+def call_polygon_network():
+    #subprocess.run(["python", "network/0xPoly.py"])
+    call_network_eth("0xPoly.py")
+
+
+
+
+def main():
+    os.system('cls')
+    print("""
+ |___  | |___  | |___  | |___  | |___  | |___  | |___  | |___  | |___ 
+    _|_|    _|_|    _|_|    _|_|    _|_|    _|_|    _|_|    _|_|    _|     ┳┓┓┏┳┓┏┓┳┳┓┳┏┳┓┏┓ 🐋
+_  | |___  | |___  | |___  | |___  | |___  | |___  | |___  | |___  | |     ┃┃┗┫┃┃┣┫┃┃┃┃ ┃ ┣  
+_|_|    _|_|    _|_|    _|_|    _|_|    _|_|    _|_|    _|_|    _|_|       ┻┛┗┛┛┗┛┗┛ ┗┻ ┻ ┗┛
+ |___  | |___  | |___  | |___  | |___  | |___  | |___  | |___  | |___         By @makaki22
+    _|_|    _|_|    _|_|    _|_|    _|_|    _|_|    _|_|    _|_|    _|
+_  | |___  | |___  | |___  | |___  | |___  | |___  | |___  | |___  | |     [ Auto Smart-Contract Recon & Analyze ]
+_|_|    _|_|    _|_|    _|_|    _|_|    _|_|    _|_|    _|_|    _|_|       [ 1. ETHEREUM | 2. AVAX | 3. BASE | 4. BSC | 5. CRONOS | 6. POLYGON ]
+          
+""")
+    
+    makakcall0x = input("select network : ")
+    print("")
+    
+    if makakcall0x == '1':
+        print("🦥 you have chosen the ETHEREUM network.")
+        call_ethereum_network()
+    elif makakcall0x == '2':
+        print("🐦 you have chosen the AVAX network.")
+        call_avax_network()
+    elif makakcall0x == '3':
+        print("🐬 you have chosen the BASE network.")
+        call_base_network()
+    elif makakcall0x == '4':
+        print("🦘 you have chosen the BSC network.")
+        call_bsc_network()
+    elif makakcall0x == '5':
+        print("🦏 you have chosen the CRONOS network.")
+        call_cronos_network()
+    elif makakcall0x == '6':
+        print("🦑 you have chosen the POLYGON network.")
+        call_polygon_network()
+    else:
+        print(Fore.WHITE + "[" + Fore.RED + "ERROR" + Fore.WHITE + "]" + " Invalid choice ! please retry.")
+
+if __name__ == "__main__":
+    main()
